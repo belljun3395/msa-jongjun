40863383232a873bb0fd5ad922a7e3bbb2d690f6
bellljun3395 <195850@jnu.ac.kr>
2022-12-08 19:08:19 +0900

    feat(auth/MemberController) : MemberController 구현

---
+++ auth/src/main/java/com/example/web/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.example.web.controller;
+
+import com.example.service.member.MemberService;
+import com.example.web.response.Response;
+import com.example.web.dto.MemberJoinDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/members")
+public class MemberController {
+
+    private final String SUCCESS_JOIN = "success join";
+
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public ResponseEntity<Response<Object>> join(@Validated MemberJoinDTO memberJoinDTO) {
+
+        memberService.join(memberJoinDTO.convertToMember());
+
+        Response<Object> success_join = makeResponse(HttpStatus.CREATED.value(), SUCCESS_JOIN);
+
+        return new ResponseEntity<Response<Object>>(success_join, HttpStatus.OK);
+    }
+
+    private static Response<Object> makeResponse(Integer status, String message) {
+        Response<Object> success_join = Response.builder()
+                .timestamp(new Timestamp(System.currentTimeMillis()))
+                .status(status)
+                .message(message)
+                .build();
+        return success_join;
+    }
+}
