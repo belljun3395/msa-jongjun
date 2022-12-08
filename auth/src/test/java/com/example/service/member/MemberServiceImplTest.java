79ace5792ab5e229c15ff77307fac2ee28bcef82
bellljun3395 <195850@jnu.ac.kr>
2022-12-08 19:08:47 +0900

    test(auth/MemberServiceImpl): MemberServiceImplTest 구현

---
+++ auth/src/test/java/com/example/service/member/MemberServiceImplTest.java
@@ -0,0 +1,94 @@
+package com.example.service.member;
+
+import com.example.domain.member.Member;
+import com.example.domain.member.MemberRepository;
+import com.example.domain.member.Role;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Sql("/data.sql")
+public class MemberServiceImplTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member member;
+
+    @Before
+    public void initData() {
+        Optional<Member> byId = memberRepository.findById(1L);
+        member = byId.get();
+    }
+
+    @Test
+    public void join() {
+        //given
+        Member member = new Member("김종준", "jong3395@gmail.com", "1234", Role.MEMBER);
+
+        //when
+        memberService.join(member);
+        Member repositoryMember = memberRepository.findById(member.getId())
+                .get();
+
+        //then
+        Assertions.assertThat(repositoryMember.getName())
+                .isEqualTo(member.getName());
+    }
+
+    @Test
+    public void adjustRoleAdmin() throws Exception {
+        //given
+
+        //when
+        memberService.adjustRole(member, Role.ADMIN);
+        Member repositoryMember = memberRepository.findById(member.getId())
+                .get();
+
+        //then
+
+        Assertions.assertThat(repositoryMember.getRole())
+                .isEqualTo(Role.ADMIN);
+    }
+
+    @Test
+    public void adjustRoleMember() throws Exception {
+        //given
+        Member getMember = memberRepository.findById(2L)
+                .get();
+
+        //when
+        memberService.adjustRole(getMember, Role.MEMBER);
+        Member repositoryMember = memberRepository.findById(getMember.getId())
+                .get();
+
+        //then
+        Assertions.assertThat(repositoryMember.getRole())
+                .isEqualTo(Role.MEMBER);
+    }
+    @Test
+    public void alreadyAdmin() throws Exception {
+        //given
+
+        //when
+        Member adminMember = memberRepository.findById(3L)
+                .get();
+
+        //then
+        Assertions.assertThatIllegalStateException()
+                .isThrownBy(() -> memberService.adjustRole(adminMember, Role.ADMIN));
+    }
+
+}
