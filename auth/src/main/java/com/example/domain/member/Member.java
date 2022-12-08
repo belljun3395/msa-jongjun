bf3b5037bb77132b0a855b618c24741e954dbed9
bellljun3395 <195850@jnu.ac.kr>
2022-12-08 19:06:50 +0900

    feat(auth/Member): Member 클래스 구현

---
+++ auth/src/main/java/com/example/domain/member/Member.java
@@ -0,0 +1,46 @@
+package com.example.domain.member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "member_name")
+    private String name;
+
+    @Column(name = "member_email")
+    private String email;
+
+    @Column(name = "member_password")
+    private String password;
+
+    @Column(name = "member_role")
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    public Member(String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Member(Member member, Role role) {
+        this.id = member.getId();
+        this.name = member.getName();
+        this.email = member.getEmail();
+        this.password = member.getPassword();
+        this.role = role;
+    }
+}
