a3be5ac5f587fee933b3ced9491466627475e6cf
bellljun3395 <195850@jnu.ac.kr>
2022-12-11 01:04:09 +0900

    refactor(auth/MemberController): join 리턴값 변경에 따른 수정

--- auth/src/main/java/com/example/web/controller/MemberController.java
+++ auth/src/main/java/com/example/web/controller/MemberController.java
@@ -1,7 +1,7 @@
 package com.example.web.controller;
 
 import com.example.service.member.MemberService;
-import com.example.web.response.Response;
+import com.example.web.response.ApiResponse;
 import com.example.web.dto.MemberJoinDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,35 +10,21 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 
-import java.sql.Timestamp;
+import javax.validation.constraints.Null;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/members")
 public class MemberController {
-
-    private final String SUCCESS_JOIN = "success join";
-
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<Response<Object>> join(@Validated MemberJoinDTO memberJoinDTO) {
-
-        memberService.join(memberJoinDTO);
-
-        Response<Object> success_join = makeResponse(HttpStatus.CREATED.value(), SUCCESS_JOIN);
-
-        return new ResponseEntity<Response<Object>>(success_join, HttpStatus.OK);
+    public ResponseEntity<ApiResponse<Null>> join(WebRequest request, @Validated MemberJoinDTO memberJoinDTO) {
+        return new ResponseEntity<>(memberService.join(memberJoinDTO)
+                .setPath(request),
+                HttpStatus.OK);
     }
 
-
-    private static Response<Object> makeResponse(Integer status, String message) {
-        Response<Object> success_join = Response.builder()
-                .timestamp(new Timestamp(System.currentTimeMillis()))
-                .status(status)
-                .message(message)
-                .build();
-        return success_join;
-    }
 }
