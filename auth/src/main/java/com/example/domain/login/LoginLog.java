1a6beb5052edcea326a944bab886cffe7bd81b98
bellljun3395 <195850@jnu.ac.kr>
2022-12-13 20:09:40 +0900

    feat(login/LoginLog): LoginLog 구현

---
+++ auth/src/main/java/com/example/domain/login/LoginLog.java
@@ -0,0 +1,48 @@
+package com.example.domain.login;
+
+import com.example.domain.member.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class LoginLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long memberId;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    private String clientType;
+
+    private String location;
+
+    private String refreshToken;
+    private Boolean logout;
+
+    private Timestamp timestamp;
+
+    @Builder
+    public LoginLog(Long memberId, Role role, String clientType, String location, String refreshToken) {
+        this.memberId = memberId;
+        this.role = role;
+        this.clientType = clientType;
+        this.location = location;
+        this.refreshToken = refreshToken;
+        this.logout = false;
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+    }
+
+    public void logut() {
+        this.logout = true;
+    }
+}
