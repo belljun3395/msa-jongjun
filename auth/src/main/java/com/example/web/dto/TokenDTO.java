26b3330a8b4d30c1d5c8ec1b421a2ccb9e8503f9
bellljun3395 <195850@jnu.ac.kr>
2022-12-15 18:16:29 +0900

    feat(auth/TokenDTO): getTokenCookie 구현

--- auth/src/main/java/com/example/web/dto/TokenDTO.java
+++ auth/src/main/java/com/example/web/dto/TokenDTO.java
@@ -1,9 +1,14 @@
 package com.example.web.dto;
 
 import lombok.Data;
+import javax.servlet.http.Cookie;
 
 @Data
 public class TokenDTO {
+
+    private String COOKIE_NAME = "access_token";
+    private String COOKIE_PATH = "/";
+    private String AUTH_TYPE = "Bearer";
 
     private String accessToken;
 
@@ -12,5 +17,17 @@ public class TokenDTO {
     public TokenDTO(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+    }
+
+    public Cookie getTokenCookie() {
+        Cookie access_token = new Cookie(COOKIE_NAME, this.getBearerRefreshToken());
+        access_token.setHttpOnly(true);
+        access_token.setSecure(true);
+        access_token.setPath(COOKIE_PATH);
+        return access_token;
+    }
+
+    private String getBearerRefreshToken() {
+        return AUTH_TYPE + getRefreshToken();
     }
 }
