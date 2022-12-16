cf65c3f9a899a480ff10237b114261464772ed04
bellljun3395 <195850@jnu.ac.kr>
2022-12-16 20:47:12 +0900

    refactor(auth): sql member 데이터, 스키마 분리 그에 따른 수정
    - 기존 데이터, 스키마 함께 있는 것을 분리
    - application.yml datasource.schema, data 수정

---
+++ auth/src/main/resources/member-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO MEMBER(member_role, member_email, member_name, member_password)
+values ('MEMBER', 'kjj395@naver.com', '김종준1', '1234');
+INSERT INTO MEMBER(member_role, member_email, member_name, member_password)
+values ('ADMIN', 'bell3395@gmail.com', '김종준2', '1234');
+INSERT INTO MEMBER(member_role, member_email, member_name, member_password)
+values ('ADMIN', 'jong3395@gmail.com', '김종준3', '1234');
