4e5bf8a60186b82af7fd9809a6dfbfcd0a68334d
bellljun3395 <195850@jnu.ac.kr>
2022-12-17 23:46:02 +0900

    feat(auth/Loginterceptor): API 요청 로그 파일 설정

---
+++ auth/src/main/java/com/example/web/interceptor/LogInterceptor.java
@@ -0,0 +1,48 @@
+package com.example.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j(topic = "API_ACCESS_APPENDER")
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        return true;
+    }
+
+//    rest API 사용할 때는 modleAndView를 불러오지 않기 때문에 필요가 없는 기능이다.
+//    @Override
+//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//        log.info("postHandle [{}]", modelAndView);
+//    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        Object logId = request.getAttribute(LOG_ID);
+
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
+    }
+}
