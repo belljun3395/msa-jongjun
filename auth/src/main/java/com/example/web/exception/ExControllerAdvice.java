41b1637b9230ecee1fa3b869bde433cb5adaf1fc
bellljun3395 <195850@jnu.ac.kr>
2022-12-18 22:27:56 +0900

    feat(auth/ExControllerAdvice): 예외 로그 처리

--- auth/src/main/java/com/example/web/exception/ExControllerAdvice.java
+++ auth/src/main/java/com/example/web/exception/ExControllerAdvice.java
@@ -22,7 +22,7 @@ public class ExControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({IllegalStateException.class})
     public ApiResponse<Null> illegalStateExHandler(IllegalStateException exception) {
-        log.info("IllegalStateException = {}", exception.getClass());
+        log.info("IllegalStateException = [{}][{}]", exception.getClass(), exception.getMessage());
         String defaultMessage = exception.getMessage();
         return new ApiResponse<>(1001, "IllegalStateException", defaultMessage);
     }
@@ -30,21 +30,23 @@ public class ExControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public List<ApiResponse<Null>> bindingExHandler(BindException exception) {
+        log.error("BindException = [{}][{}]", exception.getClass(), exception.getMessage());
         List<ApiResponse<Null>> errorResults = new ArrayList<>();
-        exception.getAllErrors().forEach(error -> {
-            FieldError fieldError = (FieldError) error;
-            String field = fieldError.getField();
-            String defaultMessage = fieldError.getDefaultMessage();
-            ApiResponse<Null> bindingException = new ApiResponse<>(1002, "BindingException", field + " 필드 입력이 필요합니다. " + defaultMessage);
-            errorResults.add(bindingException);
-        });
+        exception.getAllErrors()
+                .forEach(error -> {
+                    FieldError fieldError = (FieldError) error;
+                    String field = fieldError.getField();
+                    String defaultMessage = fieldError.getDefaultMessage();
+                    ApiResponse<Null> bindingException = new ApiResponse<>(1002, "BindingException", field + " 필드 입력이 필요합니다. " + defaultMessage);
+                    errorResults.add(bindingException);
+                });
         return errorResults;
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MemberValidateException.class})
     public ResponseEntity<ApiResponse<Null>> MemberValidateExHandler(MemberValidateException exception, WebRequest request) {
-        log.info("MemberValidateException = {}", exception.getClass());
+        log.error("MemberValidateException = [{}][{}]", exception.getClass(), exception.getMessage());
         String defaultMessage = exception.getMessage();
         ApiResponse<Null> memberValidateException = new ApiResponse<>(exception.getCode(), "MemberValidateException", defaultMessage);
         memberValidateException.setPath(request);
@@ -54,7 +56,7 @@ public class ExControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({TokenValidateException.class})
     public ResponseEntity<ApiResponse<Null>> TokenValidateExHandler(MemberValidateException exception, WebRequest request) {
-        log.info("TokenValidateException = {}", exception.getClass());
+        log.error("TokenValidateException = [{}][{}]", exception.getClass(), exception.getMessage());
         String defaultMessage = exception.getMessage();
         ApiResponse<Null> tokenValidateException = new ApiResponse<>(exception.getCode(), "MemberValidateException", defaultMessage);
         tokenValidateException.setPath(request);
@@ -64,7 +66,7 @@ public class ExControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ApiResponse<Null> exHandler(Exception exception) {
-        log.info("Exception = {}", exception.getClass());
+        log.error("Exception = [{}][{}]", exception.getClass(), exception.getMessage());
         String defaultMessage = exception.getMessage();
         return new ApiResponse<>(1000, "Exception", defaultMessage);
     }
