9c7f9f565dfd4d5f8f31ec58dec6fa1c48f41b1f
bellljun3395 <195850@jnu.ac.kr>
2022-12-19 00:16:54 +0900

    feat(zuul/ZuulPreLoggingFilter): ZuulPreLoggingFilter 구현

---
+++ zuul/src/main/java/com/example/filter/ZuulPreLoggingFilter.java
@@ -0,0 +1,54 @@
+package com.example.filter;
+
+import com.example.token.FeignValidateAccessToken;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ZuulPreLoggingFilter extends ZuulFilter {
+
+    private final FeignValidateAccessToken token;
+
+    @Override
+    public Object run() throws ZuulException {
+        String uuid = UUID.randomUUID().toString();
+        RequestContext context = RequestContext.getCurrentContext();
+        context.set("uuid", uuid);
+        HttpServletRequest request = context.getRequest();
+        String requestURI = request.getRequestURI();
+        log.info("REQUEST [{}][{}]", uuid, requestURI);
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String uri = request.getRequestURI();
+        if (uri.matches("/auth/members/.*")) {
+            return true;
+        }
+        String accessToken = request.getHeader("authorization");
+        return token.validateAccessToken(accessToken);
+    }
+}
+
