f74312c4b3b52ccfc274679c7fb0a4c8d85ec062
bellljun3395 <195850@jnu.ac.kr>
2022-12-20 00:07:41 +0900

    refactor(zuul/FeignValidateAccessToken): validateAccessToken 시그니처 변경
    - pathVariable -> header

--- zuul/src/main/java/com/example/token/FeignValidateAccessToken.java
+++ zuul/src/main/java/com/example/token/FeignValidateAccessToken.java
@@ -1,14 +1,13 @@
 package com.example.token;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(value = "auth")
 public interface FeignValidateAccessToken {
 
-    // todo change pathVariable to header
-    @RequestMapping(path = "/tokens/member/{tokenValue}")
-    boolean validateAccessToken(@PathVariable("tokenValue") String token);
+    @RequestMapping(path = "/tokens/members/validation")
+    boolean validateAccessToken(@RequestHeader("Authorization") String token);
 
 }
