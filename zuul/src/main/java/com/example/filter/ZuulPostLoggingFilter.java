c2b24cecdb6a9440b6487efad3a4c83ee9341224
bellljun3395 <195850@jnu.ac.kr>
2022-12-20 00:08:08 +0900

    refactor(zuul/ZuulPostLoggingFilter): shouldFilter 삭제

--- zuul/src/main/java/com/example/filter/ZuulPostLoggingFilter.java
+++ zuul/src/main/java/com/example/filter/ZuulPostLoggingFilter.java
@@ -1,6 +1,5 @@
 package com.example.filter;
 
-import com.netflix.util.Pair;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -9,15 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ZuulPostLoggingFilter extends ZuulFilter {
-    public static final String LOG_ID = "logId";
-
 
     @Override
     public Object run() throws ZuulException {
@@ -42,17 +37,6 @@ public class ZuulPostLoggingFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
-        // todo check is this right?
-        // todo request header authorization keep
-        RequestContext context = RequestContext.getCurrentContext();
-        List<Pair<String, String>> originResponseHeaders = context.getOriginResponseHeaders();
-        HttpServletResponse response = context.getResponse();
-        for (Pair<String, String> p : originResponseHeaders) {
-            if (p.first().equals("Authorization") || p.first().equals("Set-Cookie")) {
-                response.setHeader(p.first(), p.second());
-            }
-        }
-        context.setResponse(response);
         return true;
     }
 
