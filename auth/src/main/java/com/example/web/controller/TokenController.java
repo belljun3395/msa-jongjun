8edd5db48fd4a22e90d58e2ed4ed6bd25b96c209
bellljun3395 <195850@jnu.ac.kr>
2022-12-20 14:51:21 +0900

    refactor(auth/TokenController): 메서드 시그니처 수정

--- auth/src/main/java/com/example/web/controller/TokenController.java
+++ auth/src/main/java/com/example/web/controller/TokenController.java
@@ -5,7 +5,6 @@ import com.example.web.dto.MemberInfoDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,16 +19,13 @@ public class TokenController {
     private final AccessTokenService service;
 
     @GetMapping("/members")
-    public MemberInfoDTO browseMatchAccessToken(WebRequest request, HttpServletResponse response,
-                                                @RequestHeader("Authorization") String accessTokenValue) {
-
+    public MemberInfoDTO browseMatchAccessToken(HttpServletResponse response, @RequestHeader("Authorization") String accessTokenValue) {
         response.setHeader(AUTHORIZATION_HEADER, accessTokenValue);
         return service.browseMatchAccessToken(accessTokenValue);
     }
 
     @GetMapping("/members/validation")
-    public boolean validateAccessToken(WebRequest request) {
-        String accessTokenValue = request.getHeader(AUTHORIZATION_HEADER);
+    public boolean validateAccessToken(@RequestHeader("Authorization") String accessTokenValue) {
         service.validateAccessToken(accessTokenValue);
         return true;
     }
