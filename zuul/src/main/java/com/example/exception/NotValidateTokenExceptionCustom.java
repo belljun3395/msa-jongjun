e5c843b0dc1d63fc808f6863a3d4f48f3d43858c
bellljun3395 <195850@jnu.ac.kr>
2022-12-21 22:41:00 +0900

    feat(zuul/NotValidateTokenExceptionCustom): NotValidateTokenExceptionCustom 구현

---
+++ zuul/src/main/java/com/example/exception/NotValidateTokenExceptionCustom.java
@@ -0,0 +1,21 @@
+package com.example.exception;
+
+public class NotValidateTokenExceptionCustom extends CustomZuulException {
+    private  int code = 1002;
+
+    private String error = "NotValidateTokenException";
+
+    private String errorMessage = "Not validate token";
+
+    public  int getCode() {
+        return code;
+    }
+
+    public  String getError() {
+        return error;
+    }
+
+    public  String getErrorMessage() {
+        return errorMessage;
+    }
+}
