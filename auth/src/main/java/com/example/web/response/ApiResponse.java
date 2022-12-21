f8a1c30ac90886b875c4ecb54710c5a1687d9243
bellljun3395 <195850@jnu.ac.kr>
2022-12-22 00:49:27 +0900

    refactor(auth): api 반환 구현 수정

--- auth/src/main/java/com/example/web/response/ApiResponse.java
+++ auth/src/main/java/com/example/web/response/ApiResponse.java
@@ -1,47 +1,59 @@
 package com.example.web.response;
 
 import lombok.Getter;
-import org.springframework.web.context.request.ServletWebRequest;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Getter
-public class ApiResponse<T> {
+public class ApiResponse<T> extends ResponseEntity<T> {
 
-    private Timestamp timestamp;
-    private int code;
-    private String error;
-    private String message;
-    private T data;
-    private String path;
-
-    public ApiResponse(int code, String message, T data) {
-        this.timestamp = new Timestamp(System.currentTimeMillis());
-        this.code = code;
-        this.message = message;
-        this.data = data;
+    public ApiResponse(T body, HttpStatus status) {
+        super(body, status);
     }
 
-    public ApiResponse(int code, String message) {
-        this.timestamp = new Timestamp(System.currentTimeMillis());
-        this.code = code;
-        this.message = message;
+
+    @Getter
+    public static class FailureBody implements Serializable {
+        private Timestamp timestamp;
+        private int code;
+        private String error;
+        private String message;
+
+        public FailureBody(final int code, final String error, final String message) {
+            this.timestamp = new Timestamp(System.currentTimeMillis());
+            this.code = code;
+            this.error = error;
+            this.message = message;
+        }
     }
 
-    public ApiResponse(int code, String error, String message) {
-        this.timestamp = new Timestamp(System.currentTimeMillis());
-        this.code = code;
-        this.error = error;
-        this.message = message;
-    }
+    @Getter
+    public static class withData<T> implements Serializable {
+        private Timestamp timestamp;
+        private int code;
+        private String message;
+        private T data;
 
-    public ApiResponse<T> setPath(WebRequest webRequest) {
-        ServletWebRequest servletWebRequest = (ServletWebRequest) webRequest;
-        HttpServletRequest servletRequest = servletWebRequest.getRequest();
-        this.path = servletRequest.getRequestURI();
-        return this;
+        public withData(T data,int code, String message) {
+            this.timestamp = new Timestamp(System.currentTimeMillis());
+            this.code = code;
+            this.message = message;
+            this.data = data;
+        }
     }
+    @Getter
+    public static class withCodeAndMessage implements Serializable {
+        private Timestamp timestamp;
+        private int code;
+        private String message;
 
+        public withCodeAndMessage(int code, String message) {
+            this.timestamp = new Timestamp(System.currentTimeMillis());
+            this.code = code;
+            this.message = message;
+        }
+    }
 }
