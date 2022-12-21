5aa3f207f5a1554abed4b6243af2f5b147eac072
bellljun3395 <195850@jnu.ac.kr>
2022-12-22 01:05:17 +0900

    feat(zuul/ZuulExceptionFailureBody): makeErrorResponse 반영

--- zuul/src/main/java/com/example/filter/ZuulSendErrorFilter.java
+++ zuul/src/main/java/com/example/filter/ZuulSendErrorFilter.java
@@ -1,17 +1,16 @@
 package com.example.filter;
 
 import com.example.exception.CustomZuulException;
+import com.example.exception.ZuulExceptionFailureBody;
 import com.example.exception.NotAllowedAPIExceptionCustom;
 import com.example.exception.NotValidateTokenExceptionCustom;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
-import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.sql.Timestamp;
 
 @Slf4j
 @Component
@@ -55,17 +54,10 @@ public class ZuulSendErrorFilter extends ZuulFilter {
 
     private static void makeErrorResponse(RequestContext context, HttpServletRequest request, CustomZuulException exception) {
         context.remove("throwable");
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("timestamp", new Timestamp(System.currentTimeMillis()));
-        jsonObject.put("code", exception.getCode());
-        jsonObject.put("error", exception.getError());
-        jsonObject.put("message", exception.getErrorMessage());
-        jsonObject.put("data", null);
-        jsonObject.put("path", request.getRequestURI());
-
-        context.setResponseBody(jsonObject.toJSONString());
+        ZuulExceptionFailureBody body = new ZuulExceptionFailureBody(exception.getCode(), exception.getClass().getSimpleName(), exception.getErrorMessage());
+        context.setResponseBody(body.convertToJsonObject().toJSONString());
         context.getResponse()
                 .setContentType("application/json");
-        context.setResponseStatusCode(406);  // 406 Not Acceptable
+        context.setResponseStatusCode(406);
     }
 }
