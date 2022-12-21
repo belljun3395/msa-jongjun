6fffd6ada5be0a50dc3dbea3e68a7354a2d4b8df
bellljun3395 <195850@jnu.ac.kr>
2022-12-21 22:42:02 +0900

    feat(zuul/FeignValidateAccessToken): 요청 주소 변경 및 validateAccessTokenRole 구현
    - /tokens/members/validation -> /tokens/validation

--- zuul/src/main/java/com/example/token/FeignValidateAccessToken.java
+++ zuul/src/main/java/com/example/token/FeignValidateAccessToken.java
@@ -1,13 +1,16 @@
 package com.example.token;
 
+import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "auth", fallbackFactory = FeignValidateAccessTokenFallbackFactory.class)
 public interface FeignValidateAccessToken {
 
-    @RequestMapping(path = "/tokens/members/validation")
+    @RequestMapping(path = "/tokens/validation")
     boolean validateAccessToken(@RequestHeader("Authorization") String token);
+
+    @RequestMapping(path = "/tokens/validation/role/{role}")
+    boolean validateAccessTokenRole(@RequestHeader("Authorization") String token, @PathVariable("role") String role);
 
 }
