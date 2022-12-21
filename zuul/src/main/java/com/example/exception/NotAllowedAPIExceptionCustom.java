a866ec1db7b919f90f6f4c92022caeccecc1d0a1
bellljun3395 <195850@jnu.ac.kr>
2022-12-21 22:40:50 +0900

    feat(zuul/NotAllowedAPIExceptionCustom): NotAllowedAPIExceptionCustom 구현

---
+++ zuul/src/main/java/com/example/exception/NotAllowedAPIExceptionCustom.java
@@ -0,0 +1,25 @@
+package com.example.exception;
+
+import lombok.Getter;
+
+@Getter
+public class NotAllowedAPIExceptionCustom extends CustomZuulException {
+    private int code = 1001;
+
+    private String error = "NotAllowedApi";
+
+    private String errorMessage = "Not Allowed Api";
+
+
+    public  int getCode() {
+        return code;
+    }
+
+    public  String getError() {
+        return error;
+    }
+
+    public  String getErrorMessage() {
+        return errorMessage;
+    }
+}
