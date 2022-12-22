23cc877117adb0e38fac82a7dae92e8463e68bac
bellljun3395 <195850@jnu.ac.kr>
2022-12-22 21:11:10 +0900

    refactor(group): 메인 실행 클래스명 변경
    - Group -> GroupApplication

--- group/src/main/java/com/example/GroupApplication.java
+++ group/src/main/java/com/example/GroupApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Group {
+public class GroupApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Group.class);
+        SpringApplication.run(GroupApplication.class);
     }
 }
