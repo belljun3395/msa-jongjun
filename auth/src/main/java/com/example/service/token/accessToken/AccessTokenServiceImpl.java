2c7359e05be08435aa44acad96b94f189c3722ff
bellljun3395 <195850@jnu.ac.kr>
2022-12-27 19:35:17 +0900

    refactor(auth/AccessTokenService): 공통 구성 반영 및 makeAccessToken 로직 수정
    - validateExpirationTime -> refreshExpirationTime
    - refreshToken을 통해 accessToken 만들 때 refreshToken expirationTime 검사 및 refresh

--- auth/src/main/java/com/example/service/token/accessToken/AccessTokenServiceImpl.java
+++ auth/src/main/java/com/example/service/token/accessToken/AccessTokenServiceImpl.java
@@ -2,18 +2,16 @@ package com.example.service.token.accessToken;
 
 import com.example.domain.member.Member;
 import com.example.domain.member.MemberRepository;
-import com.example.domain.member.Role;
 import com.example.domain.token.accessToken.AccessToken;
 import com.example.domain.token.accessToken.AccessTokenRepository;
 import com.example.domain.token.accessToken.AccessTokenService;
 import com.example.utils.token.JwtToken;
+import com.example.utils.token.TokenConfig;
 import com.example.web.dto.MemberInfoDTO;
 import com.example.web.exception.MemberValidateError;
 import com.example.web.exception.MemberValidateException;
 import com.example.web.exception.TokenValidateError;
 import com.example.web.exception.TokenValidateException;
-import io.jsonwebtoken.JwtException;
-import io.lettuce.core.RedisException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,22 +21,12 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.example.domain.member.Role.*;
 import static com.example.domain.member.Role.makeRole;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AccessTokenServiceImpl implements AccessTokenService {
-
-    private final String MEMBER_ID = "memberId";
-    private final String ROLE_KEY = "role";
-    private final Long NOW = System.currentTimeMillis();
-    private final Long TWENTY_MIN = 20 * 60L * 1000L;
-
-    private final Long ACCESS_TOKEN_EXP = NOW + TWENTY_MIN;
-    private final String UUID_KEY = "uuid";
-
 
     private final AccessTokenRepository repository;
 
@@ -54,7 +42,6 @@ public class AccessTokenServiceImpl implements AccessTokenService {
     @Override
     @Transactional
     public MemberInfoDTO browseMemberMatch(String accessTokenValue) {
-        validateAccessToken(accessTokenValue);
         AccessToken accessToken = getAccessTokenBy(JwtToken.getUUID(accessTokenValue));
         Member member = getMemberBy(accessToken);
         return MemberInfoDTO.convertFrom(member);
@@ -62,36 +49,26 @@ public class AccessTokenServiceImpl implements AccessTokenService {
 
     @Override
     public AccessToken findAccessToken(String accessTokenValue) {
-        validateAccessToken(accessTokenValue);
         return getAccessTokenBy(JwtToken.getUUID(accessTokenValue));
     }
 
     @Override
     public AccessToken makeAccessToken(String refreshToken) {
-        JwtToken.validateExpirationTime(refreshToken);
-        Long memberId = Long.valueOf(JwtToken.decodeToken(refreshToken, MEMBER_ID));
-        String Role = JwtToken.decodeToken(refreshToken, ROLE_KEY);
-        AccessToken accessToken = new AccessToken(JwtToken.makeToken(ACCESS_TOKEN_EXP, this.makeUUID()), memberId, makeRole(Role));
+        JwtToken.refreshExpirationTime(refreshToken);
+        Long memberId = Long.valueOf(JwtToken.decodeToken(refreshToken, TokenConfig.MEMBERID_KEY));
+        String Role = JwtToken.decodeToken(refreshToken, TokenConfig.ROLE_KEY);
+        AccessToken accessToken =
+                new AccessToken(JwtToken.makeToken(System.currentTimeMillis() + TokenConfig.TWENTY_MIN, this.makeUUID()),
+                                                    memberId,
+                                                    makeRole(Role));
         repository.save(accessToken);
         return accessToken;
     }
 
     private Map<String, Object> makeUUID() {
         HashMap<String, Object> uuidInfo = new HashMap<>();
-        uuidInfo.put(UUID_KEY, UUID.randomUUID());
+        uuidInfo.put(TokenConfig.UUID_KEY, UUID.randomUUID());
         return uuidInfo;
-    }
-    @Override
-    @Transactional
-    public boolean validateAccessToken(String accessTokenValue) {
-        try {
-            if (!JwtToken.validateExpirationTime(accessTokenValue)) {
-                return false;
-            }
-            return true;
-        } catch (JwtException | RedisException e) {
-            return false;
-        }
     }
 
     private AccessToken getAccessTokenBy(String accessTokenValue) {
