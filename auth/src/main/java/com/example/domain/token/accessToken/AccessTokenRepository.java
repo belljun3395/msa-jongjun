f50102078d2538dfb08ad56866b97ee5bfe91455
bellljun3395 <195850@jnu.ac.kr>
2023-01-01 20:32:15 +0900

    refactor(auth): AccessToken 관련 삭제

--- auth/src/main/java/com/example/domain/token/accessToken/AccessTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.domain.token.accessToken;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccessTokenRepository extends CrudRepository<AccessToken, String> {
-}
-
--- auth/src/main/java/com/example/web/controller/TokenController.java
+++ auth/src/main/java/com/example/web/controller/TokenController.java
@@ -1,52 +1,51 @@
-package com.example.web.controller;
-
-import com.example.domain.token.accessToken.AccessToken;
-import com.example.domain.token.accessToken.AccessTokenService;
-import com.example.utils.token.JwtToken;
-import com.example.web.dto.MemberInfoDTO;
-import com.example.web.response.ApiResponse;
-import com.example.web.response.ApiResponseGenerator;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-
-
-@Slf4j
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/tokens")
-public class TokenController {
-
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private final AccessTokenService service;
-
-    @GetMapping
-    public ApiResponse<ApiResponse.withData> accessTokenInfo(@RequestHeader(AUTHORIZATION_HEADER) String access_token) {
-        AccessToken accessToken = service.findAccessToken(access_token);
-        return ApiResponseGenerator.success(accessToken, HttpStatus.OK, 1100, "member info");
-    }
-
-    @GetMapping("/access")
-    public AccessToken makeAccessToken(@CookieValue String refresh_token) {
-        return service.makeAccessToken(refresh_token);
-    }
-
-    @GetMapping("/members")
-    public MemberInfoDTO browseMemberMatch(HttpServletResponse response, @RequestHeader(AUTHORIZATION_HEADER) String accessTokenValue) {
-        response.setHeader(AUTHORIZATION_HEADER, accessTokenValue);
-        return service.browseMemberMatch(accessTokenValue);
-    }
-
-    @GetMapping("/validation")
-    public boolean validateAccessToken(@RequestHeader(AUTHORIZATION_HEADER) String accessTokenValue) {
-        return JwtToken.validateToken(accessTokenValue);
-    }
-
-    @GetMapping("/validation/role/{role}")
-    public boolean validateAccessTokenRole(@RequestHeader(AUTHORIZATION_HEADER) String accessTokenValue, @PathVariable String role) {
-        return service.validateAccessTokenRole(accessTokenValue, role);
-    }
-}
+//package com.example.web.controller;
+//
+//import com.example.domain.token.accessToken.AccessToken;
+//import com.example.domain.token.accessToken.AccessTokenService;
+//import com.example.web.dto.MemberInfoDTO;
+//import com.example.web.response.ApiResponse;
+//import com.example.web.response.ApiResponseGenerator;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/tokens")
+//public class TokenController {
+//
+//    private static final String AUTHORIZATION_HEADER = "Authorization";
+//    private final AccessTokenService service;
+//
+//    @GetMapping
+//    public ApiResponse<ApiResponse.withData> accessTokenInfo(@RequestHeader(AUTHORIZATION_HEADER) String access_token) {
+//        AccessToken accessToken = service.findAccessToken(access_token);
+//        return ApiResponseGenerator.success(accessToken, HttpStatus.OK, 1100, "member info");
+//    }
+//
+//    @GetMapping("/access")
+//    public AccessToken makeAccessToken(@CookieValue String refresh_token) {
+//        return service.makeAccessToken(refresh_token);
+//    }
+//
+//    @GetMapping("/members")
+//    public MemberInfoDTO browseMemberMatch(HttpServletResponse response, @RequestHeader(AUTHORIZATION_HEADER) String accessTokenValue) {
+//        response.setHeader(AUTHORIZATION_HEADER, accessTokenValue);
+//        return service.browseMemberMatch(accessTokenValue);
+//    }
+//
+//    @GetMapping("/validation")
+//    public boolean validateAccessToken(@RequestHeader(AUTHORIZATION_HEADER) String accessTokenValue) {
+//        return JwtToken.validateToken(accessTokenValue);
+//    }
+//
+//    @GetMapping("/validation/role/{role}")
+//    public boolean validateAccessTokenRole(@RequestHeader(AUTHORIZATION_HEADER) String accessTokenValue, @PathVariable String role) {
+//        return service.validateAccessTokenRole(accessTokenValue, role);
+//    }
+//}
