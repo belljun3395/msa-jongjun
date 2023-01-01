0b16c297a6ab8e34887663e38178261ecdf56b72
bellljun3395 <195850@jnu.ac.kr>
2023-01-02 00:05:05 +0900

    refactor: 의존성 정리 및 반영
    - auth
    - group

--- auth/src/main/java/com/example/AuthApplication.java
+++ auth/src/main/java/com/example/AuthApplication.java
@@ -2,14 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableEurekaClient
-@EnableFeignClients
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class);
